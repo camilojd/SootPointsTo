c6c2fc0423d62753aafa24b2def2f3a807b36103
Julian Jamardo <jamardo.julian@gmail.com>
2015-05-14 21:01:30 -0300

    Implementamos FlowInfo con los datos que necesitamos pasarle a Soot

--- PointsTo/src/dc/aap/PtgForwardAnalysis.java
+++ PointsTo/src/dc/aap/PtgForwardAnalysis.java
@@ -1,20 +1,30 @@
 package dc.aap;
 
+import java.util.Dictionary;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import soot.*;
-import java.util.Iterator;
-import soot.util.*;
-import java.util.*;
+
+import org.apache.commons.lang3.builder.*;
+
+//import org.jgrapht.graph.DefaultEdge;
+
 import soot.jimple.*;
 import soot.Unit;
 import soot.Value;
-import soot.ValueBox;
+//import soot.ValueBox;
 import soot.toolkits.graph.DirectedGraph;
-import soot.toolkits.scalar.ArraySparseSet;
-import soot.toolkits.scalar.FlowSet;
+//import soot.toolkits.scalar.ArraySparseSet;
+//import soot.toolkits.scalar.FlowSet;
 import soot.toolkits.scalar.ForwardFlowAnalysis;
 
-public class PtgForwardAnalysis extends ForwardFlowAnalysis{
+//import org.jgrapht.*;
+//import org.jgrapht.graph.*;
 
+public class PtgForwardAnalysis extends ForwardFlowAnalysis{
+	
 	public PtgForwardAnalysis(DirectedGraph<Unit> graph) {
 		super(graph);
 		doAnalysis();
@@ -23,6 +33,12 @@ public class PtgForwardAnalysis extends ForwardFlowAnalysis{
 	@Override
 	protected void flowThrough(Object in, Object u, Object out) {
 		Unit d = (Unit)u;
+		
+		FlowInfo flowInfo = (FlowInfo)in;
+		flowInfo.nodes.add("pepe");
+		flowInfo.nodes.add("pepe2");
+		flowInfo.edges.add(new Edge("pepe", "f", "pepe2"));
+		System.out.println(flowInfo.edges);
 		
 		if (!d.getUseBoxes().isEmpty() && !d.getDefBoxes().isEmpty()) {
 			Value right = d.getUseBoxes().get(0).getValue();
@@ -49,12 +65,18 @@ public class PtgForwardAnalysis extends ForwardFlowAnalysis{
 			//	System.out.println("Es un call()");
 			// Esto, por algun motivo, no anda
 		}
+		
+		flowInfo.edges.remove((new Edge("pepe", "f", "pepe2")));
+		System.out.println(flowInfo.edges);
+		
+		out = flowInfo;
+		
 		System.out.println("================");
 	}
 
 	@Override
-	protected FlowSet<Value> newInitialFlow() {
-		return new ArraySparseSet<Value>();
+	protected Object newInitialFlow() {
+		return new FlowInfo();
 	}
 
 	@Override
@@ -67,4 +89,59 @@ public class PtgForwardAnalysis extends ForwardFlowAnalysis{
 		//source.copy(dest);
 	}
 
+}
+
+class FlowInfo {
+	FlowInfo() {
+		nodes = new HashSet<String>();
+		edges = new HashSet<Edge>();
+		locals = new HashMap<String, String>();
+		wrongs = new HashSet<String>();
+	}
+	public Set<String> nodes;
+	public Set<Edge> edges;
+	public Map<String, String> locals;
+	public Set<String> wrongs;
+}
+
+class Edge {
+	Edge(String source, String field, String target) {
+		vSource = source;
+		vTarget = target;
+		this.field = field;
+	}
+	String vSource;
+	String vTarget;
+	String field;
+	
+	@Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 31). // two randomly chosen prime numbers
+            // if deriving: appendSuper(super.hashCode()).
+            append(vSource).
+            append(vTarget).
+            append(field).
+            toHashCode();
+    }
+	
+	@Override
+    public boolean equals(Object obj) {
+       if (!(obj instanceof Edge))
+            return false;
+        if (obj == this)
+            return true;
+
+        Edge edge = (Edge) obj;
+        return new EqualsBuilder().
+            // if deriving: appendSuper(super.equals(obj)).
+            append(vTarget, edge.vTarget).
+            append(vSource, edge.vSource).
+            append(field, edge.field).
+            isEquals();
+    }
+
+	@Override
+	public String toString() {
+		return vSource + " " + field  + " " + vTarget ;  
+	}
 }
